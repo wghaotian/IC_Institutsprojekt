30d22225cb6e6d901cbdbabf59266d9ae6a4fe77
wghaotian <34622764+wghaotian@users.noreply.github.com>
2022-06-13 00:24:40 +0200

    Update von Pfingstwoche
    1. Einige Bugs mit geschriebene Funktionen fixiert, und funktionen über Datenraten implementiert.
    2. Modell vom Basisstation über Stateflow Chart aufgebaut, aber noch nicht getestet (siehe State_BS.slx)
    
    Diese woche können wir versuchen, die Daten aus main Workspace und die Simulink (insbesondere mit Stateflow Machine/Chart) zu synchronisieren (z.B. mithilfe von Event Broadcasting), und den Aufbau sowie Update von Q-Matrix zu implementieren.
    
    Mehr Informationen über Stateflow Chart: https://de.mathworks.com/help/stateflow/gs/stateflow-charts.html

--- Consumer.m
+++ Consumer.m
@@ -3,7 +3,6 @@ classdef Consumer < SimulationsObject
         data_demand=0;
         v=[0,0];
         spawn_time=0;
-        despawn_time=0;
     end
     
     events
@@ -13,38 +12,29 @@ classdef Consumer < SimulationsObject
     
     methods
   %% Constructor
-        function obj=Consumer(x,y,name,dmd,vx,vy,arr_t,leav_t)
+        function obj=Consumer(x,y,name,dmd,vx,vy,arr_t)
             obj@SimulationsObject(x,y,name);
             obj.data_demand=dmd;
             obj.v=[vx,vy];
             obj.spawn_time=arr_t;
-            obj.despawn_time=leav_t;
         end
   %% set speed      
         function obj=setSpeed(obj,vx,vy)
             obj.v=[vx,vy];
         end
-  %% Plotting function    
-        function plotCU(obj,axis)
-            xy = obj.pos;
-            plot(axis,xy(1), xy(2), 'kx');
-        end
  %% find the nearest base station  
- % Q: Ignore the capacity of base stations?
- % Q: Or should we also try to allocate the channel capacity?
         function [index,minBS]=nearestBS(obj, BS_List)
-           min_dist=inf;
-           index=1;
-           I=1;
-           for BS=BS_List
-               if (CalcLength(BS,obj)<min_dist)
-                   minBS=BS;
-                   index=I;
-                   min_dist=CalcLength(BS,obj);
-               end
-               I=I+1;
+           num_BS=size(BS_List,2);
+           dist=zeros(num_BS);
+           for I=(1:num_BS)
+               dist(I)=obj.CalcLength(BS_List(I));
            end
+           [~,index]=min(dist);
+           minBS=BS_List(index);
         end
+        
+
+        
 %% Data Request 
         function Arrive(obj,time)
             notify(obj,'arrive');
--- Map.m
+++ Map.m
@@ -4,27 +4,24 @@ classdef Map
         BS_List % Base Station List
         CS_List % Consumer List
         map_size % [x,y] Map Size
-        total_Time% total time 
-        max_Cap % maximum channel capacity in Mbps
+        total_Time % total time 
+
     end
     
     methods
-        function obj = Map(x,y,total_Time,max_Cap)
+        function obj = Map(x,y,total_Time)
             %Map Construct an instance of this class
             obj.BS_List=BaseStation.empty;
             obj.CS_List=Consumer.empty;
             obj.map_size=[x,y];
             obj.total_Time=total_Time;
-            obj.max_Cap=max_Cap;
         end
         
         function obj = add_item(obj,obj1)
-            n_BS=size(obj.BS_List,2);
-            n_CS=size(obj.CS_List,2);
             if (isa(obj1,'BaseStation'))
-                obj.BS_List(n_BS+1)=obj1;
+                obj.BS_List=[obj.BS_List;obj1];
             elseif (isa(obj1,'Consumer'))
-                obj.CS_List(n_CS+1)=obj1;
+                obj.CS_List=[obj.CS_List;obj1];
             end
         end
         
@@ -32,21 +29,28 @@ classdef Map
             numBS=size(pos,1);
             for I=(1:numBS)
                 nameI=['BS ',num2str(size(obj.BS_List,2)+1)];
-                obj=obj.add_item(BaseStation(pos(I,:),nameI,1));% Set each base station to active at first
+                obj=obj.add_item(BaseStation(pos(I,1),pos(I,2),nameI,1));% Set each base station to active at first
             end
         end
         
         function obj = add_CS(obj)% Add 1 random consumer
             % Question: Should consumers' arrival be equally distributed or
             % otherwise (i.e. normal, lognormal, etc.) distributed???
-            % Gleichverteilung finde ich ein bisschen sinnlos... 
+            % Gleichverteilung scheint ein bisschen sinnlos... 
+            % First equally distribution to simplify the simulation process
+            % and then find some distribution to it
+            
+            % Leave time depends on the data demand and the data rate of
+            % the base station which is connected to.
+
+            
             x=rand()*obj.map_size(1);
             y=rand()*obj.map_size(2);
             nameCS=['CS ',num2str(size(obj.CS_List,2)+1)];
-            arr_t=rand()*obj.total_Time;
-            leav_t=arr_t+rand()*(obj.total_Time-arr_t);
-            data=rand()*obj.max_Cap;
-            CS=Consumer(x,y,nameCS,data,0,0,arr_t,leav_t);
+            arr_t=rand()*obj.total_Time; % to be fixed
+            global conf;
+            data=wblrnd(conf.lamda_scale,conf.k_shape);
+            CS=Consumer(x,y,nameCS,data,0,0,arr_t);
             obj=obj.add_item(CS);
         end
         
@@ -55,10 +59,32 @@ classdef Map
             CS_spawn_list=obj.CS_List(ind);
         end
         
+        function cg=channel_gain(obj,BS_ind,CS_ind)
+            BS=obj.BS_List(BS_ind);
+            CS=obj.CS_List(CS_ind);
+            cg=128.1+37.6*log10(CalcLength(BS,CS));
+            cg=10^(-cg/10);
+        end
         
-        function CS_despawn_list = sort_CS_despawn(obj)
-            [~,ind]=sort([obj.CS_List.spawn_time]);
-            CS_despawn_list=obj.CS_List(ind);
+        function snr=SINR(obj,BS_ind,CS_ind)
+            n=size(obj.BS_List,2);
+            global conf;
+            snr=10^(2*(conf.sigma_noise/10));
+            
+            for I=[(1:BS_ind-1),(BS_ind+1:n)]
+                if (obj.BS_List(I).sleepMode~=0) 
+                    continue
+                else
+                    snr=snr+conf.BS_Pow*channel_gain(obj,I,CS_ind);
+                end
+            end
+            snr=conf.BS_Pow*channel_gain(obj,BS_ind,CS_ind)/snr;
+        end
+        
+        function rate=data_rate(obj,BS_ind,CS_ind)
+            global conf;
+            alpha=1/size(obj.BS_List(BS_ind).serveList,2);
+            rate=alpha*conf.W_band*log2(1+SINR(obj,BS_ind ,CS_ind));
         end
         
     end
