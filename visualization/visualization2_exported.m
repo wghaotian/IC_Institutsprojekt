06048e65de870d7df73d23422cb3c93636ad9445
Oliver-Sch <oliver.schirrmacher@rwth-aachen.de>
2022-06-16 18:46:39 +0200

    Zwischenstand
    Kurzes Zwischenbackup
    
    An Aus Knopf

--- visualization/visualization2_exported.m
+++ visualization/visualization2_exported.m
@@ -2,29 +2,27 @@ classdef visualization2_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure                        matlab.ui.Figure
-        GridLayout                      matlab.ui.container.GridLayout
-        LeftPanel                       matlab.ui.container.Panel
-        GModellSimulationLabel          matlab.ui.control.Label
-        SelectSceneLabel                matlab.ui.control.Label
-        SelectSceneDropDown             matlab.ui.control.DropDown
-        StartStopSimulationSwitchLabel  matlab.ui.control.Label
-        StartStopSimulationSwitch       matlab.ui.control.Switch
-        Image                           matlab.ui.control.Image
-        Label                           matlab.ui.control.Label
-        Label2                          matlab.ui.control.Label
-        StartAppButton                  matlab.ui.control.Button
-        SelectSceneDropDown_2Label      matlab.ui.control.Label
-        SelectSceneDropDown_2           matlab.ui.control.DropDown
-        StartAppButton_2                matlab.ui.control.Button
-        RightPanel                      matlab.ui.container.Panel
-        SimulatedtimeLabel              matlab.ui.control.Label
-        LastactionsTextAreaLabel        matlab.ui.control.Label
-        LastactionsTextArea             matlab.ui.control.TextArea
-        EditField                       matlab.ui.control.NumericEditField
-        placeholderLabel                matlab.ui.control.Label
-        Panel                           matlab.ui.container.Panel
-        UIAxes                          matlab.ui.control.UIAxes
+        UIFigure                  matlab.ui.Figure
+        GridLayout                matlab.ui.container.GridLayout
+        LeftPanel                 matlab.ui.container.Panel
+        GModellSimulationLabel    matlab.ui.control.Label
+        TestSceneDropDownLabel    matlab.ui.control.Label
+        TestSceneDropDown         matlab.ui.control.DropDown
+        StopResumeSimulationSwitchLabel  matlab.ui.control.Label
+        Image                     matlab.ui.control.Image
+        Label                     matlab.ui.control.Label
+        Label2                    matlab.ui.control.Label
+        StartTestAppButton        matlab.ui.control.Button
+        ShowTestsCheckBox         matlab.ui.control.CheckBox
+        ONButton                  matlab.ui.control.StateButton
+        RightPanel                matlab.ui.container.Panel
+        SimulatedtimeLabel        matlab.ui.control.Label
+        LastactionsTextAreaLabel  matlab.ui.control.Label
+        LastactionsTextArea       matlab.ui.control.TextArea
+        EditField                 matlab.ui.control.NumericEditField
+        placeholderLabel          matlab.ui.control.Label
+        Panel                     matlab.ui.container.Panel
+        UIAxes                    matlab.ui.control.UIAxes
     end
 
     % Properties that correspond to apps with auto-reflow
@@ -71,52 +69,67 @@ classdef visualization2_exported < matlab.apps.AppBase
     % Callbacks that handle component events
     methods (Access = private)
 
-        % Button pushed function: StartAppButton
-        function StartAppButtonPushed(app, event)
+        % Button pushed function: StartTestAppButton
+        function StartTestAppButtonPushed(app, event)
             if (app.countStart ~= 0)
                 f = cla(app.gx,'reset');
                 f.Children;
                 app.LastactionsTextArea.Value = "Panel cleared" + newline + app.LastactionsTextArea.Value;
             end
-            app.gx = geoaxes(app.Panel);
-            geobasemap(app.gx,'streets-light')
-            hold(app.gx,'on')
+%             app.gx = geoaxes(app.Panel);
+%             geobasemap(app.gx,'streets-light')
+%             hold(app.gx,'on')
             app.countStart = app.countStart +1 ;
         end
 
-        % Value changed function: SelectSceneDropDown
-        function SelectSceneDropDownValueChanged(app, event)
-            value = app.SelectSceneDropDown.Value;
+        % Value changed function: TestSceneDropDown
+        function TestSceneDropDownValueChanged(app, event)
+            value = app.TestSceneDropDown.Value;
             if (app.countStart == 0)
-                app.LastactionsTextArea.Value = "Drücke zuerst auf den Start Knopf!" + newline + app.LastactionsTextArea.Value;
+                app.LastactionsTextArea.Value = "Dr�cke zuerst auf den Start Knopf!" + newline + app.LastactionsTextArea.Value;
             elseif (value == '2' )
-                latAachen = 50.775555;
-                lonAachen = 6.083611;
-                latKoeln = 50.935173;
-                lonKoeln = 6.953101;
-                geoplot(app.gx,[latAachen latKoeln],[lonAachen lonKoeln],'g-*')
+                % Add background
+                imshow('map_background.png','Parent',app.UIAxes);
             elseif (value == '3' )
-                latAachen = 50.755555;
-                lonAachen = 6.083611;
-                latKoeln = 50.935173;
-                lonKoeln = 6.953101;
-                geoplot(app.gx,[latAachen latKoeln],[lonAachen lonKoeln],'g-*')
+                % Add map
+                
             elseif (value == '4' )
-                latAachen = 50.775555;
-                lonAachen = 6.083611;
-                %annotation(app.gx,'textarrow',[latAachen latAachen-0.002],[latAachen latAachen],'String','Aachen');
-                text(app.gx,latAachen,lonAachen,'\leftarrow Aachen');
-                latKoeln = 50.935173;
-                lonKoeln = 6.953101;
-                text(app.gx,latKoeln,lonKoeln,'\leftarrow Koeln');
+                % Add BS
+                
+            elseif (value == '5' )
+                % Add CU
+                
+            elseif (value == '6' )
+                % Remove CU
+                
             end
+%             elseif (value == '2' )
+%                 latAachen = 50.775555;
+%                 lonAachen = 6.083611;
+%                 latKoeln = 50.935173;
+%                 lonKoeln = 6.953101;
+%                 geoplot(app.gx,[latAachen latKoeln],[lonAachen lonKoeln],'g-*')
+%             elseif (value == '3' )
+%                 latAachen = 50.755555;
+%                 lonAachen = 6.083611;
+%                 latKoeln = 50.935173;
+%                 lonKoeln = 6.953101;
+%                 geoplot(app.gx,[latAachen latKoeln],[lonAachen lonKoeln],'g-*')
+%             elseif (value == '4' )
+%                 latAachen = 50.775555;
+%                 lonAachen = 6.083611;
+%                 %annotation(app.gx,'textarrow',[latAachen latAachen-0.002],[latAachen latAachen],'String','Aachen');
+%                 text(app.gx,latAachen,lonAachen,'\leftarrow Aachen');
+%                 latKoeln = 50.935173;
+%                 lonKoeln = 6.953101;
+%                 text(app.gx,latKoeln,lonKoeln,'\leftarrow Koeln');
         end
 
-        % Value changed function: SelectSceneDropDown_2
+        % Callback function
         function SelectSceneDropDown_2ValueChanged(app, event)
             value = app.SelectSceneDropDown_2.Value;
             if (app.countStart == 0)
-                app.LastactionsTextArea.Value = "Drücke zuerst auf den Start Knopf!" + newline + app.LastactionsTextArea.Value;
+                app.LastactionsTextArea.Value = "Dr�cke zuerst auf den Start Knopf!" + newline + app.LastactionsTextArea.Value;
             elseif (value == '2' )
                 latAachen = 50.0;
                 lonAachen = 6.0;
@@ -126,7 +139,7 @@ classdef visualization2_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: StartAppButton_2
+        % Callback function
         function StartAppButton_2Pushed(app, event)
             if (app.countStart ~= 0)
                 f = cla(app.gx,'reset');
@@ -136,6 +149,32 @@ classdef visualization2_exported < matlab.apps.AppBase
             app.gx = app.UIAxes;
             hold(app.gx,'on')
             app.countStart = app.countStart +1 ;
+        end
+
+        % Value changed function: ShowTestsCheckBox
+        function ShowTestsCheckBoxValueChanged(app, event)
+            value = app.ShowTestsCheckBox.Value;
+            if (value == 1)
+                app.StartTestAppButton.Visible = 1;
+                app.TestSceneDropDown.Visible = 1;
+                app.TestSceneDropDownLabel.Visible = 1;
+            elseif (value == 0)
+                app.StartTestAppButton.Visible = 0;
+                app.TestSceneDropDown.Visible = 0;
+                app.TestSceneDropDownLabel.Visible = 0;
+            end
+        end
+
+        % Value changed function: ONButton
+        function ONButtonValueChanged(app, event)
+            value = app.ONButton.Value;
+            if (value == 1)
+                app.ONButton.BackgroundColor = [0.00,1.00,0.00];
+                app.ONButton.Text = 'ON';
+            elseif (value == 0)
+                app.ONButton.BackgroundColor = [1.00,0.00,0.00];
+                app.ONButton.Text = 'OFF';
+            end
         end
 
         % Changes arrangement of the app based on UIFigure width
@@ -190,29 +229,27 @@ classdef visualization2_exported < matlab.apps.AppBase
             app.GModellSimulationLabel.Position = [46 665 132 22];
             app.GModellSimulationLabel.Text = '5G- Modell Simulation';
 
-            % Create SelectSceneLabel
-            app.SelectSceneLabel = uilabel(app.LeftPanel);
-            app.SelectSceneLabel.HorizontalAlignment = 'right';
-            app.SelectSceneLabel.Position = [11 582 80 22];
-            app.SelectSceneLabel.Text = 'Select Scene:';
+            % Create TestSceneDropDownLabel
+            app.TestSceneDropDownLabel = uilabel(app.LeftPanel);
+            app.TestSceneDropDownLabel.HorizontalAlignment = 'right';
+            app.TestSceneDropDownLabel.Visible = 'off';
+            app.TestSceneDropDownLabel.Position = [20 111 69 22];
+            app.TestSceneDropDownLabel.Text = 'Test-Scene:';
 
-            % Create SelectSceneDropDown
-            app.SelectSceneDropDown = uidropdown(app.LeftPanel);
-            app.SelectSceneDropDown.Items = {'Nothing', 'AC-Mitte -> Köln', 'AC-Süd -> Köln', 'Label Citys'};
-            app.SelectSceneDropDown.ItemsData = {'1', '2', '3', '4'};
-            app.SelectSceneDropDown.ValueChangedFcn = createCallbackFcn(app, @SelectSceneDropDownValueChanged, true);
-            app.SelectSceneDropDown.Position = [106 582 100 22];
-            app.SelectSceneDropDown.Value = '1';
+            % Create TestSceneDropDown
+            app.TestSceneDropDown = uidropdown(app.LeftPanel);
+            app.TestSceneDropDown.Items = {'Nothing', 'Add Background', 'Add Map', 'Add BS', 'Add CU', 'Remove CU'};
+            app.TestSceneDropDown.ItemsData = {'1', '2', '3', '4', '5', '6'};
+            app.TestSceneDropDown.ValueChangedFcn = createCallbackFcn(app, @TestSceneDropDownValueChanged, true);
+            app.TestSceneDropDown.Visible = 'off';
+            app.TestSceneDropDown.Position = [104 111 100 22];
+            app.TestSceneDropDown.Value = '1';
 
-            % Create StartStopSimulationSwitchLabel
-            app.StartStopSimulationSwitchLabel = uilabel(app.LeftPanel);
-            app.StartStopSimulationSwitchLabel.HorizontalAlignment = 'center';
-            app.StartStopSimulationSwitchLabel.Position = [23 545 66 28];
-            app.StartStopSimulationSwitchLabel.Text = {'Start / Stop'; 'Simulation'};
-
-            % Create StartStopSimulationSwitch
-            app.StartStopSimulationSwitch = uiswitch(app.LeftPanel, 'slider');
-            app.StartStopSimulationSwitch.Position = [133 549 45 20];
+            % Create StopResumeSimulationSwitchLabel
+            app.StopResumeSimulationSwitchLabel = uilabel(app.LeftPanel);
+            app.StopResumeSimulationSwitchLabel.HorizontalAlignment = 'center';
+            app.StopResumeSimulationSwitchLabel.Position = [16 627 85 28];
+            app.StopResumeSimulationSwitchLabel.Text = {'Stop / Resume'; 'Simulation'};
 
             % Create Image
             app.Image = uiimage(app.LeftPanel);
@@ -223,40 +260,35 @@ classdef visualization2_exported < matlab.apps.AppBase
             app.Label = uilabel(app.LeftPanel);
             app.Label.FontSize = 7.9;
             app.Label.Position = [102 11 115 31];
-            app.Label.Text = {'Andrej Fadin, Haotian Wang, '; 'Huiying Zhang, Marc Wagels, '; 'Oliver Schirrmacher, Till Müller'};
+            app.Label.Text = {'Andrej Fadin, Haotian Wang, '; 'Huiying Zhang, Marc Wagels, '; 'Oliver Schirrmacher, Till M�ller'};
 
             % Create Label2
             app.Label2 = uilabel(app.LeftPanel);
             app.Label2.FontSize = 8;
             app.Label2.FontWeight = 'bold';
             app.Label2.Position = [14 37 203 42];
-            app.Label2.Text = {'Institutsprojekt: '; 'Maschinelles Lernen in der Kommunikationstechnik '; 'und Verteilte Algorithmen für adaptive Schlafmodi '; 'in 5G-Netzen'};
+            app.Label2.Text = {'Institutsprojekt: '; 'Maschinelles Lernen in der Kommunikationstechnik '; 'und Verteilte Algorithmen f�r adaptive Schlafmodi '; 'in 5G-Netzen'};
 
-            % Create StartAppButton
-            app.StartAppButton = uibutton(app.LeftPanel, 'push');
-            app.StartAppButton.ButtonPushedFcn = createCallbackFcn(app, @StartAppButtonPushed, true);
-            app.StartAppButton.Position = [66 625 100 22];
-            app.StartAppButton.Text = 'Start App';
+            % Create StartTestAppButton
+            app.StartTestAppButton = uibutton(app.LeftPanel, 'push');
+            app.StartTestAppButton.ButtonPushedFcn = createCallbackFcn(app, @StartTestAppButtonPushed, true);
+            app.StartTestAppButton.Visible = 'off';
+            app.StartTestAppButton.Position = [59 140 100 22];
+            app.StartTestAppButton.Text = 'Start Test-App';
 
-            % Create SelectSceneDropDown_2Label
-            app.SelectSceneDropDown_2Label = uilabel(app.LeftPanel);
-            app.SelectSceneDropDown_2Label.HorizontalAlignment = 'right';
-            app.SelectSceneDropDown_2Label.Position = [14 382 80 22];
-            app.SelectSceneDropDown_2Label.Text = 'Select Scene:';
+            % Create ShowTestsCheckBox
+            app.ShowTestsCheckBox = uicheckbox(app.LeftPanel);
+            app.ShowTestsCheckBox.ValueChangedFcn = createCallbackFcn(app, @ShowTestsCheckBoxValueChanged, true);
+            app.ShowTestsCheckBox.Text = 'Show Tests?';
+            app.ShowTestsCheckBox.Position = [13 82 90 22];
 
-            % Create SelectSceneDropDown_2
-            app.SelectSceneDropDown_2 = uidropdown(app.LeftPanel);
-            app.SelectSceneDropDown_2.Items = {'Nothing', 'AC-Mitte -> Köln'};
-            app.SelectSceneDropDown_2.ItemsData = {'1', '2'};
-            app.SelectSceneDropDown_2.ValueChangedFcn = createCallbackFcn(app, @SelectSceneDropDown_2ValueChanged, true);
-            app.SelectSceneDropDown_2.Position = [109 382 100 22];
-            app.SelectSceneDropDown_2.Value = '1';
-
-            % Create StartAppButton_2
-            app.StartAppButton_2 = uibutton(app.LeftPanel, 'push');
-            app.StartAppButton_2.ButtonPushedFcn = createCallbackFcn(app, @StartAppButton_2Pushed, true);
-            app.StartAppButton_2.Position = [66 424 100 22];
-            app.StartAppButton_2.Text = 'Start App';
+            % Create ONButton
+            app.ONButton = uibutton(app.LeftPanel, 'state');
+            app.ONButton.ValueChangedFcn = createCallbackFcn(app, @ONButtonValueChanged, true);
+            app.ONButton.Text = 'ON';
+            app.ONButton.BackgroundColor = [0 1 0];
+            app.ONButton.Position = [110 630 100 22];
+            app.ONButton.Value = true;
 
             % Create RightPanel
             app.RightPanel = uipanel(app.GridLayout);
@@ -288,7 +320,7 @@ classdef visualization2_exported < matlab.apps.AppBase
             % Create placeholderLabel
             app.placeholderLabel = uilabel(app.RightPanel);
             app.placeholderLabel.Position = [10 14 342 98];
-            app.placeholderLabel.Text = {'Bekannte Bugs:'; '1.'; 'Nach jeder Aktion sollte eigentlich ein Zeilensprung '; 'stattfinden'};
+            app.placeholderLabel.Text = {'Bekannte Bugs:'; 'X'};
 
             % Create Panel
             app.Panel = uipanel(app.RightPanel);
@@ -300,7 +332,10 @@ classdef visualization2_exported < matlab.apps.AppBase
             title(app.UIAxes, 'Title')
             xlabel(app.UIAxes, 'X')
             ylabel(app.UIAxes, 'Y')
-            app.UIAxes.Position = [1 -1 725 483];
+            app.UIAxes.XLim = [0 1000];
+            app.UIAxes.YLim = [0 750];
+            app.UIAxes.ZLim = [0 100];
+            app.UIAxes.Position = [1 -1 726 483];
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
