de64a598ad48c9eb1bc9870d2310bbcaea8795ac
Oliver-Sch <oliver.schirrmacher@rwth-aachen.de>
2022-06-16 20:16:08 +0200

    Update maptest.m

--- visualization/maptest.m
+++ visualization/maptest.m
@@ -1,11 +1,3 @@
 %% Run to start GUI
 
-% gx = geoaxes;
-% latAachen = 50.775555;
-% lonAachen = 6.083611;
-% latKoeln = 50.935173;
-% lonKoeln = 6.953101;
-% geoplot(gx,[latAachen latKoeln],[lonAachen lonKoeln],'g-*')
-% geobasemap(gx,'topographic')
-
 visualization2;
